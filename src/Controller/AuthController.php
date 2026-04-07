f6da1c81db8faa4ba729a5e4d9884fa9f4c409af
Uthman-T <wakfabrio15@gmail.com>
2026-04-07 12:02:09 +0200

    fix: sql injection

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Core\Controller;
+use App\Repository\UserRepository;
 
 class AuthController extends Controller
 {
@@ -15,8 +16,8 @@ class AuthController extends Controller
      * */
     public function __construct()
     {
-        $username = 'formation';
-        $password = 'paris';
+        $username = 'uthman';
+        $password = 'formation';
         $host = '127.0.0.1';
         $port = 3306;
         $dbname = 'php_poo';
@@ -56,10 +57,14 @@ class AuthController extends Controller
              * @info
              * "' OR 1=1 -- ": la chaine suivante ajoute une condition qui est toujours vrai et commente la suite de la query.
              * */
-            $query = "SELECT * FROM `user` WHERE email='$email' AND password='$password'";
-            $result = $this->conn->query($query);
+            $userRepository = new UserRepository();
+            $user = $userRepository->findByEmail($email);
 
-            if ($result && mysqli_num_rows($result) > 0) {
+            if (!$user) {
+                throw new \Exception('User not found', 404);
+            }
+
+            if (password_verify($password, $user->getPassword())) {
                 if (session_status() === PHP_SESSION_NONE) {
                     session_start();
                 }
